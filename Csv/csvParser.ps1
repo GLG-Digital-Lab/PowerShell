f06a16fe3126c5fa21f44a852ab687ff0557136f
GLG-Digital-Lab <legac.glenn@gmail.com>
2018-02-23 16:50:58 +0100

    Support

---
+++ Csv/csvParser.ps1
@@ -0,0 +1,20 @@
+﻿While ($true) {
+    $userInput = Read-Host "Quel est votre numéro de département ?"
+    $csv = Get-Content -Path "C:\Users\Aston\Desktop\Powershell\departement.csv" 
+    $i = 0;
+    foreach ( $data in  $csv ) {
+        $d = $data.Split(",")
+        $id = $d[1].replace('"','')
+        $departement = $d[2].replace('"','')
+        if ($id  -eq $userInput) {
+            echo "`n+---------------------------------------------+"
+            echo "| Votre département est : $departement "
+            echo "+---------------------------------------------+`n"
+            $i++;
+        }
+    }
+    if ($i -eq 0) {
+        echo "Le numéro de département n'est pas reconnu."
+    }
+}
+ 
